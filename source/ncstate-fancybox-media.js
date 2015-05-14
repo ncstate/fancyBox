d0091f8b996c4efd0868ed896b074970afbb542e
Andrew Matthews <apmatthe@ncsu.edu>
2015-05-14 17:11:07 -0400

    additional customization for media helper

--- source/ncstate-fancybox-media.js
+++ source/ncstate-fancybox-media.js
@@ -7,10 +7,13 @@ $(document).ready(function() {
             openEffect : 'none',
             closeEffect : 'none',
             arrows : false,
+            width: 800,
+            height: 450,
             padding : 0,
             tpl : {
                 closeBtn : '',
             },
+            title: false,
             helpers : {
                 media : {}
             }
