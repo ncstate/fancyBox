f4d10e3b8d2000f6a2186fedc9fa1e66b5eafcb5
Andrew Matthews <apmatthe@ncsu.edu>
2016-08-10 17:45:09 -0400

    combine base and media fancybox; add single image option

--- source/ncstate-fancybox.js
+++ source/ncstate-fancybox.js
@@ -27,4 +27,27 @@ $(document).ready(function() {
             }
         });
 
+    $(".fancybox-magnify").each(function(index, img) {
+        $(img).fancybox({
+            beforeLoad: function() {
+                this.title = $(this.element).attr('data-caption');
+            },
+            openEffect : 'none',
+            closeEffect : 'none',
+            nextEffect: 'fade',
+            prevEffect: 'fade',
+            padding : 0,
+            tpl : {
+                closeBtn : '<a title="Close" class="fancybox-item fancybox-close" href="javascript:;"><span class="glyphicon glyphicon-x"></span></a>',
+                next     : '<a title="Next" class="fancybox-nav fancybox-next"><span class="glyphicon glyphicon-right-arrow"></span></a>',
+                prev     : '<a title="Previous" class="fancybox-nav fancybox-prev"><span class="glyphicon glyphicon-left-arrow"></span></a>'
+            },
+            helpers : {
+                title : {
+                    type : 'inside'
+                }
+            }
+        });
+    });
+
 });
