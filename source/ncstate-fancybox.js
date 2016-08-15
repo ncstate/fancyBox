cee41e7449715144b0c5f2220c739d271b3cfbe2
Andrew Matthews <apmatthe@ncsu.edu>
2016-08-15 16:08:15 -0400

    add fancybox media hook to js

--- source/ncstate-fancybox.js
+++ source/ncstate-fancybox.js
@@ -27,6 +27,25 @@ $(document).ready(function() {
             }
         });
 
+    if ($(window).width() > 768) {
+       $('.fancybox-media')
+        .fancybox({
+            openEffect : 'none',
+            closeEffect : 'none',
+            arrows : false,
+            width: 800,
+            height: 450,
+            padding : 0,
+            tpl : {
+                closeBtn : '',
+            },
+            title: false,
+            helpers : {
+                media : {}
+            }
+        });
+    }
+
     $(".fancybox-magnify").each(function(index, img) {
         $(img).fancybox({
             beforeLoad: function() {
